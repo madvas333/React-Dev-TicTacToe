dbd17925483bb2a231d88c18a2227d65d529a4ee
Madison Vasquez <madvas333@gmail.com>
2024-07-31 12:43:55 -0700

    adding a winner function to game

--- src/App.js
+++ src/App.js
@@ -6,12 +6,22 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] =useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
-function handleClick (i) {
-  const nextSquares = squares.slice();
-  nextSquares[i] = "X";
-  setSquares(nextSquares);
-}
+
+  function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
 
   return (
     <>
