80e3f5a19fb23dd1d81319ef714a93f72ed70994
Madison Vasquez <madvas333@gmail.com>
2024-08-10 14:15:00 -0700

    add new pages

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NewPage() {
+  return (
+    <div className="new-page">
+      <h1>Welcome to the New Page!</h1>
+      <p>This is where you can add whatever you like.</p>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Game from './Game';
+import Home from './Home';
+import NewPage from './NewPage'; // Import your fabulous new page!
 
-import App from "./App";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/newpage" element={<NewPage />} /> {/* Add the new page route here! */}
+      </Routes>
+    </Router>
+  );
+}
 
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
