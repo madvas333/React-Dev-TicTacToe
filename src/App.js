fdeaf81e31d13b364b426558bc364454056be162
Madison Vasquez <madvas333@gmail.com>
2024-08-11 15:43:41 -0700

    update react and react router versions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-// App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Checkers from './Checkers';
+import TicTacToe from './TicTacToe';
+import NavBar from './NavBar'
 
 function App() { 
   return (
@@ -9,8 +12,8 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/checkers" element={<Checkers />} />
-          <Route path="/tictactoe" element={<TicTacToe />} />
+          <Route path="/Checkers" element={<Checkers />} />
+          <Route path="/TicTacToe" element={<TicTacToe />} />
         </Routes>
       </Router>
     </div>  
--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export default function NewPage() {
+export default function Home() {
   return (
-    <div className="new-page">
+    <div className="Home">
       <h1>Welcome to the New Page!</h1>
       <p>This is where you can add whatever you like.</p>
     </div>
