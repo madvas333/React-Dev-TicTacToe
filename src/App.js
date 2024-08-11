121bfe82292d926feaaeba025412ead3db3d96d3
Madison Vasquez <madvas333@gmail.com>
2024-08-11 15:17:37 -0700

    taking out app declaration in two spots

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Checkers from './Checkers';
-import TicTacToe from './TicTacToe';
-import NavBar from './NavBar';
 
-function App() {
+function App() { 
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/checkers" element={<Checkers />} />
-        <Route path="/tictactoe" element={<TicTacToe />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/checkers" element={<Checkers />} />
+          <Route path="/tictactoe" element={<TicTacToe />} />
+        </Routes>
+      </Router>
+    </div>  
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Checkers from './Checkers';
-import TicTacToe from './TicTacToe';
-import NavBar from './NavBar'
-
-function App() {
-  return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Checkers" element={<Checkers />} />
-        <Route path="/TicTacToe" element={<TicTacToe />} /> {/* Add the new page route here! */}
-      </Routes>
-    </Router>
-  );
-}
+import App from './App';  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
