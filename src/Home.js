f7c21be9c3d97ba351c9f9d6afcbde049be03687
Madison Vasquez <madvas333@gmail.com>
2024-08-12 15:47:41 -0700

    adding styling to the navbar and the homepage

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import './styles.css'; // Make sure to link your CSS file
 
 export default function Home() {
   return (
-    <div className="Home">
-      <h1>Welcome to the New Page!</h1>
-      <p>This is where you can add whatever you like.</p>
+    <div className="home">
+      <h1>Tadaa</h1>
+      <p>This is the home page.....</p>
+      <h2>Tadaa</h2>
+      <p>testing out some random stuff in css</p>
+      <h3>Tadaa</h3>
+      <p className="special-paragraph">seeing how the styling changes</p>
+
     </div>
   );
 }
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './styles.css'; // Make sure the correct CSS file is linked
 
 export default function NavBar() {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav className="navbar"> {/* Apply the navbar class here */}
+      <ul className="navbar-list">
+        <li className="navbar-item">
           <Link to="/">Home</Link>
         </li>
-        <li>
-          <Link to="/checkers">Checkers</Link>
+        <li className="navbar-item">
+          <Link to="/Checkers">Checkers</Link>
         </li>
-        <li>
-          <Link to="/tictactoe">Tic-Tac-Toe</Link>
+        <li className="navbar-item">
+          <Link to="/TicTacToe">Tic-Tac-Toe</Link>
         </li>
       </ul>
     </nav>
